e3e91e7742c673781486e30f2f885b7b915cb0e5
Your Name <you@example.com>
2023-01-24 16:49:58 -0300

    rotas dos alunos prontas

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,10 +1,109 @@
 import Aluno from "../models/Aluno";
 
-class AlunoController{
-    async index(req, res){
-      const alunos = await Aluno.findAll()
-      res.json(alunos)
+class AlunoController {
+  async index(req, res) {
+    const alunos = await Aluno.findAll();
+    res.json(alunos);
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.status(200).json({
+        msg: "aluno criado com sucesso",
+        aluno: aluno,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
     }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Id não informado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      return res.status(200).json(aluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Id não informado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      await aluno.destroy();
+      return res.status(200).json("Aluno deletado com sucesso");
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ["Id não informado"],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ["Aluno não existe"],
+        });
+      }
+
+      const novoAluno = await aluno.update(req.body);
+
+      return res.status(200).json({
+        msg: "aluno atualizado com sucesso",
+        aluno: novoAluno,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import alunoController from '../controllers/AlunoController'
+import alunoController from "../controllers/AlunoController";
+import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
 
-router.get('/', alunoController.index)
+router.get("/", loginRequired, alunoController.index); // retorna todos os alunos
+router.post("/", loginRequired, alunoController.store); // cria um aluno
+router.put("/:id", loginRequired, alunoController.update); // atualiza um aluno
+router.get("/:id", loginRequired, alunoController.show); // mostra um aluno
+router.delete("/:id", loginRequired, alunoController.delete); // deleta um aluno
 
 export default router;
